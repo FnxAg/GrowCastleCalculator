7dc0388e573acb666fa591bc247b5138300f75cf
FnxAg <2303499521@qq.com>
2025-10-25 00:54:45 +0800

    update GoldCalculator page

--- lib/pages/tool_pages/gold_calculator.dart
+++ lib/pages/tool_pages/gold_calculator.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:grow_castle_calculator/pages/calculator_page.dart';
 
-class GoldCalculator extends StatelessWidget {
+class GoldCalculator extends StatefulWidget {
   const GoldCalculator({super.key});
+
+  @override
+  State<GoldCalculator> createState() => _GoldCalculatorState();
+}
+
+class _GoldCalculatorState extends State<GoldCalculator> {
+  bool _goldCalculatorWaveBox = true;
+
+  final List<TextEditingController> _goldCalculatorWaveController = [
+    TextEditingController(),
+    TextEditingController(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _initWaveValue();
+  }
+
+  void _initWaveValue() {
+    if (!_goldCalculatorWaveBox) return;
+    for (int i = 0; i < _goldCalculatorWaveController.length; i++) {
+      _goldCalculatorWaveController[i].text = CalculatorPage.waveValue[i]
+          .toString();
+    }
+  }
+
+  @override
+  void dispose() {
+    for (var controller in _goldCalculatorWaveController) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +51,118 @@ class GoldCalculator extends StatelessWidget {
           icon: const Icon(Icons.arrow_back),
         ),
       ),
-      body: const Center(child: Text('Gold Calculator')),
+      // body: const Center(child: Text('Gold Calculator')),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          spacing: 8,
+          children: [
+            Row(
+              spacing: 8,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Tooltip(
+                    message: 'Toggle Wave Value',
+                    child: Checkbox(
+                      value: _goldCalculatorWaveBox,
+                      onChanged: (value) {
+                        setState(() {
+                          _goldCalculatorWaveBox = value!;
+                          if (_goldCalculatorWaveBox) {
+                            _initWaveValue();
+                          }
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 5,
+                  child: TextFormField(
+                    controller: _goldCalculatorWaveController[0],
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    readOnly: _goldCalculatorWaveBox ? true : false,
+                    decoration: InputDecoration(
+                      labelText: 'Current Wave',
+                      hintText: 'Enter Current Wave',
+                      border: const OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 4,
+                  child: TextFormField(
+                    controller: _goldCalculatorWaveController[1],
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    readOnly: _goldCalculatorWaveBox ? true : false,
+                    decoration: InputDecoration(
+                      labelText: 'Seasonal Wave',
+                      hintText: 'Enter Seasonal Wave',
+                      border: const OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              spacing: 8,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Game Speed',
+                      hintText: 'Enter Game Speed',
+                      border: const OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Jump + Wave',
+                      hintText: 'Enter Wave',
+                      border: const OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Wave Time (s)',
+                      hintText: 'Enter Wave Time',
+                      border: const OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              spacing: 8,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'IC Level',
+                      hintText: 'Enter Infinite Colony Level',
+                      border: const OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
