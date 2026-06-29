93bc928b9f01deecb8c6e062edcf6655e0d1362a
FnxAg <2303499521@qq.com>
2026-06-29 17:27:23 +0800

    release v1.0.9

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 12
-        versionName = "1.0.8"
+        versionCode = 13
+        versionName = "1.0.9"
     }
 
     signingConfigs {
